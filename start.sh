b232834a45c9d05778a4f4fd08d4b241557b2043
rjgallac <rjgallac@hotmail.com>
2026-03-14 11:20:09 +0000

    load lms studio and model as well

--- start.sh
+++ start.sh
@@ -3,3 +3,4 @@ docker start cv-compare-rabbitmq
 ./nx serve backend1 > backend1.log &
 ./nx serve backend2 > backend2.log &
 ./nx serve events-frontend > events-frontend.log &
+lms load qwen3.5-35b-a3b
